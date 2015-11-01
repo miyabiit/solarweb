594cc2b28ed1dd49a04c43ba2d0859eee84a62ff
baker@BALLOONGA <yamada.masahiro@gmail.com>
2015-11-02 00:33:54 +0900

    now day statics for each solars.

--- client/app/day/day.controller.js
+++ client/app/day/day.controller.js
@@ -2,11 +2,15 @@
 
 angular.module('solarwebApp')
   .controller('DayCtrl', function ($scope, $http) {
+		$scope.solars = [];
 		$scope.summary = [];
 		$scope.datetime_str = '';
+
+		$http.get('/api/days/month/201508').success(function(solars){
+			$scope.solars = solars;
+		});
 		
 		$http.get('/api/days').success(function(summaries){
-			var total = 0;
 			$scope.summary = summaries;
 		});
 	});
--- server/api/day/day.controller.js
+++ server/api/day/day.controller.js
@@ -3,6 +3,7 @@
 var _ = require('lodash');
 var Day = require('./day.model');
 var Summary = require('./../summary/summary.model');
+var Solar = require('./../solar/solar.model');
 
 // Get list of days
 exports.index = function(req, res) {
@@ -10,6 +11,17 @@ exports.index = function(req, res) {
 		if(err){ return handleError(res, err); }
 		return res.json(200, summary);
 	});
+};
+
+// Get list of days of any month
+exports.month = function(req, res) {
+	console.log(req.params.ym + '*');
+	var re = new RegExp(req.params.ym);
+	Solar.find({'status' : 'day','date_time' : re }, function (err, solars) {
+	//Solar.find({'status' : 'day' , 'date_time' : /201508/ }, function (err, solars) {
+		if(err) { return handleError(res, err); }
+    return res.json(200, solars);
+  });
 };
 
 // Get a single day
